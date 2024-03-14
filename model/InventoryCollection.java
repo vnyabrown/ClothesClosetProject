7de979da3ec4759a23854f9a940c1c78307d84c8
vnyabrown <vnyabrown@gmail.com>
2024-03-14 13:46:38 -0400

    InventoryCollection update (needs queries)

--- model/InventoryCollection.java
+++ model/InventoryCollection.java
@@ -22,8 +22,29 @@ public class InventoryCollection extends EntityBase implements IView{
     private void executeQueryAndPopulate(String query)
     {
         Vector allDataRetrieved = getSelectQueryResult(query);
-        
-    }
+
+        // Check against query condition to retrieve matching Inventory Items
+        for (int i = 0; i < allDataRetrieved.size(); i++) {
+            Properties nextInventory = (Properties) allDataRetrieved.elementAt(i);
+
+            // Add to our inventory list
+            Inventory inv_item = new Inventory(nextInventory);
+            inventoryList.add(inv_item);
+        }
+    } // end of executeQuereAndPopulate
+
+    // Will need queries below-- what condition do we find inventory items by?
+
+    /* Display each Inventory information from Inventory in Collection to user */
+	// ==============================================================
+	public void displayCollection()
+	{
+        // Cycle through each Patron in the Patron collection
+		for (int count = 0; count < inventoryList.size(); count++)
+        {
+            System.out.println(inventoryList.elementAt(count).toString()); // Convert each Book information to a string and display it
+        }
+	}
 
     @Override
     public void updateState(String key, Object value) {
