18f9869dd9b71b953e6b37ed739103448db23a6f
rinzler <rinzlerdes@gmail.com>
2024-03-14 12:35:24 -0400

    Got insert into db for article type working.

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import model.*;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Properties atp = new Properties();
+
+        atp.setProperty("description", "Somthing new");
+        atp.setProperty("barcodePrefix", "ab");
+        atp.setProperty("alphaCode", "cd");
+
+        ArticleType at = new ArticleType(atp);
+        at.updateStateInDatabase();
+        System.out.println(at.toString());
     }
 }
---
+++ model/ArticleType.java
@@ -0,0 +1,92 @@
+// specify the package
+package model;
+
+// system imports
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+
+// project imports
+import exception.InvalidPrimaryKeyException;
+//import impresario.IView;
+
+public class ArticleType extends EntityBase {
+    private static final String myTableName = "ArticleType";
+    protected Properties dependencies;
+    // GUI Components
+    private String updateStatusMessage = "empty from ArticleType";
+
+    // Cronstructor
+    public ArticleType(Properties articleTypeProperties) {
+        super(myTableName);
+        // Need to figure the purpose of this out
+        // setDependencies();
+        persistentState = new Properties();
+        articleTypeProperties.forEach((key, value) -> {
+            if(value != null) {
+                //System.out.println(value); // Debugging 
+                persistentState.setProperty((String)key, (String)value);
+            }
+        });                                                                                                                                                                                         
+        // Set enum for status automatically to "Active"
+        persistentState.setProperty("status", "Active");
+    }
+
+
+    public void updateStateInDatabase() {
+        try {
+            // Upate Article Type
+            if(persistentState.getProperty("id") != null) {
+                // TODO
+            }
+            else {
+                Integer id = insertAutoIncrementalPersistentState(mySchema, persistentState);
+                persistentState.setProperty("id", Integer.toString(id));
+                updateStatusMessage = "Article Type for id: " +  
+                    persistentState.getProperty("id") + 
+                    " installed successfully in database!";
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+
+    public String toString() {
+        return //"Article id: " + persistentState.getProperty("id") + "\n" +
+            "Description: " + persistentState.getProperty("description") + "\n" +
+            "Barcode: " + persistentState.getProperty("barcodePrefix") + "\n" +
+            "Alpha Code: " + persistentState.getProperty("alphaCode") + "\n" + 
+            "Status: " + persistentState.getProperty("status");
+    }
+
+
+    protected void initializeSchema(String tableName) {
+		if (mySchema == null)
+		{
+			mySchema = getSchemaInfo(tableName);
+		}
+	}
+
+
+    public Object getState(String key) {
+		if (key.equals("UpdateStatusMessage") == true)
+			return updateStatusMessage;
+
+		return persistentState.getProperty(key);
+	}
+
+
+    public void stateChangeRequest(String key, Object value) {
+
+		myRegistry.updateSubscribers(key, this);
+	}
+
+
+	/** Called via the IView relationship */
+	//----------------------------------------------------------
+	public void updateState(String key, Object value) {
+		stateChangeRequest(key, value);
+	}
+}
