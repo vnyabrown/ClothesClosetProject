06f690747e4046c83e85c40b3437cf81d9fc7167
vnyabrown <vnyabrown@gmail.com>
2024-03-26 13:46:47 -0400

    create testColor to implement testing functionalities

---
+++ testColor.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+import model.Color;
+import model.ColorCollection;
+
+import java.util.Properties;
+import database.*;
+
+//java -cp mariadb-java-client-3.0.3.jar;classes;. ATM
+//To run Java FX
+public class testColor
+{
+    public static void main(String[] args) {
+
+    } // end of main
+
+    public static void addColor()
+    {
+
+    } // end of addColor
+
+    public static void modifyColor()
+    {
+        // Requires getting a Color Collection
+        
+    } // end of modifyColor
+
+    public static void deleteColor()
+    {
+
+    } // end of deleteColor
+} // end of class
