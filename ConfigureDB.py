a418e18c02dec06fdba3715dee9249a89eb56d00
rinzler <rinzlerdes@gmail.com>
2024-03-29 17:32:01 -0400

    added python script to get rid of repeatedly changing dbConfig.ini

---
+++ ConfigureDB.py
@@ -0,0 +1,31 @@
+import os
+local_username = "ENTER YOUR USERNAME HERE"
+local_password = "ENTER YOUR LOCAL PASSWORD HERE"
+local_db_name = "ENTER YOUR LOCAL DATABASE NAME HERE"
+server = "localhost"
+
+choice = int(input("Enter 1 to connect to your local database\nEnter 2 to connect to CSDB\n"))
+
+file_path = "dbConfig.ini"
+
+if choice == 1 or choice == 2:
+    with open(file_path, "w") as file:
+        if choice == 1:
+            file.write("username=" + local_username)
+            file.write("\npassword=" + local_password)
+            file.write("\ndbName=" + local_db_name)
+            file.write("\nserver=" + server)
+            print("dbConfig.ini updated for localhost")
+        else:
+            file.write("username=" + "ahadd1")
+            file.write("\npassword=" + "123")
+            file.write("\ndbName=" + "spr24_csc429_ahadd1")
+            file.write("\nserver=" + "csdb.brockport.edu")
+            print("dbConfig.ini updated for CSDB")
+        file.close
+else:
+    print("Not a choice")
+
+
+
+
