6f3a7ead4e7afb78d1494aa1d662408aad24f329
noahm <noah.matlik@gmail.com>
2024-04-04 13:38:17 -0400

    condensed ViewFactory if/else blocks into switch statement

---
+++ out/production/GroupClosetProject/README.md
@@ -0,0 +1,8 @@
+# ClothesClosetProject
+CSC429 Term Project <br/>
+Members: Naya Brown, Olaoluwa Olojede, Ayden Haddad, Tamer Ozkum, Noah Matlik
+
+To use:
+1 - clone repository
+2 - install dependencies (ClothesClosetProject.iml)
+3 - Run Launcher class
--- userinterface/ViewFactory.java
+++ userinterface/ViewFactory.java
@@ -20,56 +20,36 @@ public class ViewFactory {
 
 	public static View createView(String viewName, IModel model)
 	{
-		if(viewName.equals("ClosetView") == true)
-		{
-			return new ClosetView(model);
-		}
-		else if(viewName.equals("ArticleChoiceView") == true)
-		{
-			return new ArticleChoiceView(model);
-		}
-		else if (viewName.equals("ColorChoiceView") == true)
-		{
-			return new ColorChoiceView(model);
-		}
-		else if (viewName.equals("InsertArticleView") == true)
-		{
-			return new InsertArticleView(model);
-		}
-		else if (viewName.equals("InsertColorView") == true)
-		{
-			return new InsertColorView(model);
-		}
-		else if (viewName.equals("DeleteArticleView") == true)
-		{
-			return new DeleteArticleView(model);
-		}
-		else if(viewName.equals("SearchArticleTypeView")) {
-			return new SearchArticleTypeView(model);
-		}
-		else if(viewName.equals("ArticleTypeCollectionView")) {
-			return new ArticleTypeCollectionView(model);
-		}
-		else if (viewName.equals("ModifyArticleTypeView") == true)
-		{
-			return new ModifyArticleTypeView(model);
-		}
-		else if (viewName.equals("DeleteColorView") == true)
-		{
-			return new DeleteColorView(model);
-		}
-		else if(viewName.equals("SearchColorView")) {
-			return new SearchColorView(model);
-		}
-		else if(viewName.equals("ColorCollectionView")) {
-			return new ColorCollectionView(model);
-		}
-		else if(viewName.equals("ModifyColorView")) {
-			return new ModifyColorView(model);
-		}
-		else {
-			System.out.println("No view found matching " + viewName + " in view factory.");
-			return null;
+		switch (viewName) {
+			case "ClosetView":
+				return new ClosetView(model);
+			case "ArticleChoiceView":
+				return new ArticleChoiceView(model);
+			case "ColorChoiceView":
+				return new ColorChoiceView(model);
+			case "InsertArticleView":
+				return new InsertArticleView(model);
+			case "InsertColorView":
+				return new InsertColorView(model);
+			case "DeleteArticleView":
+				return new DeleteArticleView(model);
+			case "SearchArticleTypeView":
+				return new SearchArticleTypeView(model);
+			case "ArticleTypeCollectionView":
+				return new ArticleTypeCollectionView(model);
+			case "ModifyArticleTypeView":
+				return new ModifyArticleTypeView(model);
+			case "DeleteColorView":
+				return new DeleteColorView(model);
+			case "SearchColorView":
+				return new SearchColorView(model);
+			case "ColorCollectionView":
+				return new ColorCollectionView(model);
+			case "ModifyColorView":
+				return new ModifyColorView(model);
+			default:
+				System.out.println("No view found matching " + viewName + " in view factory.");
+				return null;
 		}
 	}
 
