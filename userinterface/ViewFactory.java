3660c83845ea90070b40f47414e276ebb7eb4fa3
noahm <noah.matlik@gmail.com>
2024-04-04 13:50:06 -0400

    removed necessary components from .gitignore

--- userinterface/ViewFactory.java
+++ userinterface/ViewFactory.java
@@ -5,19 +5,6 @@ import impresario.IModel;
 //==============================================================================
 public class ViewFactory {
 
-	/* public static View createView(String viewName, IModel model)
-	{
-		return switch (viewName) {
-			case "ClosetView" -> new ClosetView(model);
-			case "ArticleChoiceView" -> new ArticleChoiceView(model);
-			case "ColorChoiceView" -> new ColorChoiceView(model);
-			case "InsertArticleView" -> new InsertArticleView(model);
-			case "ModifyArticleView" -> new ModifyArticleView(model);
-			case "DeleteArticleView" -> new DeleteArticleView(model);
-			default -> null;
-		};
-	} */
-
 	public static View createView(String viewName, IModel model)
 	{
 		switch (viewName) {
